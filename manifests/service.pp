b738f60f6301c6ec1535bb88eb64cf2103017459
Graham Gilbert <graham.gilbert@airbnb.com>
2017-09-20 15:20:27 -0700

    Use a watchpath to allow for an escrow attempt immediately

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,11 @@
 # Managed Crypt's serivce
 class crypt::service {
+
+  service {'com.grahamgilbert.crypt_watchpath':
+    ensure => 'running',
+    enable => true
+  }
+
   service { 'com.grahamgilbert.crypt':
     ensure => 'running',
     enable => true,
